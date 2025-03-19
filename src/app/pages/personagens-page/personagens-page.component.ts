7a21da232b20db4713850b87b21e09f0624b24a1
Jeferson Ribeiro dos Santos <jeferson.santos@amcom.com.br>
2025-03-19 20:04:48 -0300

    Primeiro projeto Angular

---
+++ src/app/pages/personagens-page/personagens-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-personagens-page',
+  templateUrl: './personagens-page.component.html',
+  styleUrl: './personagens-page.component.css'
+})
+export class PersonagensPageComponent {
+
+}
